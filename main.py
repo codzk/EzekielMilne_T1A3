c7b91a7bd1150c5fad802979997bede5818db8ac
codzk <zekemilne@gmail.com>
2023-12-12 17:07:38 +1000

    first commit, started with pseudo

---
+++ main.py
@@ -0,0 +1,18 @@
+# introduction/title (Calorie Calculator)
+
+# user 1.create profile, 2. log in and 3.exit 
+'''
+1. create profile (Username: , Password:  )
+2. log in
+3. exit 
+'''
+# Create Profile
+# Log in 
+'''hello message to user (Welcome! Username! What can I help?)
+1. Calculate Calories
+2. History Log
+3. Exit 
+'''
+
+# Calculate Calories 
+
