fc538cc5afd602dd2ecc4d47734772c765049670
codzk <zekemilne@gmail.com>
2023-12-17 20:31:27 +0800

    adding dry coding

--- app_functions.py
+++ app_functions.py
@@ -1,17 +1,19 @@
 import csv
 
-# user create profile
+def get_user_input(prompt):
+    return input(prompt)
+
 def create_profile(file_name):
     print("Create Profile")
-    username = input("Enter your username: ")
-    password = input("Enter your password: ")
+    username = get_user_input("Enter your username: ")
+    password = get_user_input("Enter your password: ")
 
     with open(file_name, "a") as f:
         writer = csv.writer(f)
         writer.writerow([username, password])
 
     print(f"Profile for {username} created successfully")
-    
+
 def load_user_data(file_name):
     user_data = []
     with open(file_name, "r") as r:
@@ -26,44 +28,30 @@ def login_valid(username, password, user_data):
             return True
     return False
 
-
-# user log in details
 def log_in(file_name):
     print("Log In")
 
     user_data = load_user_data(file_name)
 
-    
-    username = input("Enter your username: ")
-    password = input("Enter your password: ")
+    username = get_user_input("Enter your username: ")
+    password = get_user_input("Enter your password: ")
 
     if login_valid(username, password, user_data):
         print("Login Successful.")
-        return{"username": username, "password": password}
+        return {"username": username, "password": password}
     else:
         print("\nInvalid Username or Password. Please try again.")
         return None
-    
+
 def calculate_calories():
     print("Calculate Calories")
 
-    # user input for age, height, weight and activity level
-    age = int(input("Enter your age: "))
-    weight = float(input("Enter your weight in kilograms: "))
-    height = float(input("Enter your height in centimeters: "))
+    age = int(get_user_input("Enter your age: "))
+    weight = float(get_user_input("Enter your weight in kilograms: "))
+    height = float(get_user_input("Enter your height in centimeters: "))
+    activity_level = get_activity_level()
 
-    # choose an activity level
-    print("Select your activity level: ")
-    print("1. Sedentary (little or no exercise)")
-    print("2. Lightly active (light exercise/sports 1-3 days/week)")
-    print("3. Moderately active (moderate exercise/sports 3-5 days/week)")
-    print("4. Very active (hard exercise/sports 6-7 days a week)")
-    activity_level = int(input("Enter the number corresponding to your activity level: "))
-
-    # Harris-Benedict equation for estimating Basal Metabolic Rate (BMR)
-    # BMR calculation depends on gender (1.2 for females, 1.55 for males)
-
-    gender = input("Enter your gender (M/F) in upper case: ").upper()
+    gender = get_user_input("Enter your gender (M/F) in upper case: ").upper()
 
     if gender == "M":
         bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
@@ -72,42 +60,43 @@ def calculate_calories():
     else:
         print("Invalid gender. Please enter 'M' or 'F'.")
         return
-    
-    # Adjust BMR based on activity level
+
     activity_factors = [1.2, 1.375, 1.55, 1.725]
     activity_multiplier = activity_factors[activity_level - 1]
     total_calories = round(bmr * activity_multiplier)
 
     print(f"\nYour estimated daily calorie needs: {total_calories} calories.")
-    return input("\nDo you want to go back to the sub menu? (yes/no): ").lower() == 'yes'
-    # goes back to sub_option
+    return get_user_input("\nDo you want to go back to the sub menu? (yes/no): ").lower() == 'yes'
 
 def calculate_bmi():
     print("Calculate BMI")
 
-    # user input for weight and height
-    weight = float(input("Enter your weight in kilograms: "))
-    height_cm = float(input("Enter your height in centimeters: "))
-
-    #convert height to metres
+    weight = float(get_user_input("Enter your weight in kilograms: "))
+    height_cm = float(get_user_input("Enter your height in centimeters: "))
     height_m = height_cm / 100
 
-    # BMI calculation
     bmi = weight / (height_m ** 2)
 
-    print (f"\nYour BMI is: {bmi:.2f}")
+    print(f"\nYour BMI is: {bmi:.2f}")
 
-    # BMI categories
-    if bmi < 18.5:
-        category = "Underweight"
-    elif 18.5 <= bmi < 24.9:
-        category = "Normal weight"
-    elif 25 <= bmi < 29.9:
-        category = "Overweight"
-    else:
-        category = "Obese"
+    category = determine_bmi_category(bmi)
     print(f"Your BMI category is: {category}")
-    return input("\nDo you want to go back to the sub menu? (yes/no): ").lower() == 'yes'
-    #goes back to sub_option
+    return get_user_input("\nDo you want to go back to the sub menu? (yes/no): ").lower() == 'yes'
 
+def get_activity_level():
+    print("Select your activity level: ")
+    print("1. Sedentary (little or no exercise)")
+    print("2. Lightly active (light exercise/sports 1-3 days/week)")
+    print("3. Moderately active (moderate exercise/sports 3-5 days/week)")
+    print("4. Very active (hard exercise/sports 6-7 days a week)")
+    return int(get_user_input("Enter the number corresponding to your activity level: "))
 
+def determine_bmi_category(bmi):
+    if bmi < 18.5:
+        return "Underweight"
+    elif 18.5 <= bmi < 24.9:
+        return "Normal weight"
+    elif 25 <= bmi < 29.9:
+        return "Overweight"
+    else:
+        return "Obese"
--- main.py
+++ main.py
@@ -4,10 +4,8 @@ file_name = "user_data.csv"
 logged_in_user = None
 logged_in = False
 
-# introduction/title (Calorie and BMI Calculator)
 print("\nWelcome to our Calorie and BMI Calculator. Your guide for your daily calorie needs.")
 
-# menu 1. create profile, 2. log in, and 3. exit
 while True:
     print("\n1. Create Profile")
     print("2. Log In")
@@ -20,29 +18,26 @@ while True:
         logged_in_user = log_in(file_name)
         if logged_in_user:
             logged_in = True
-            # options after successful login
-            while logged_in:  # Use the logged_in flag to control the flow
+            while logged_in:
                 print("\nWelcome! How can I help?")
                 print("1. Calculate Calories")
                 print("2. Calculate BMI")
-                print("3. Log")
-                print("4. Exit")
+                print("3. Exit")
                 sub_option = input("\nEnter your choice: ")
                 if sub_option == "1":
                     if not calculate_calories():
-                        logged_in = False  # Set the flag to False to exit the inner loop
+                        logged_in = False
                 elif sub_option == "2":
                     if not calculate_bmi():
                         logged_in = False
                 elif sub_option == "3":
-                    print("For log/history")
-                elif sub_option == "4":
                     print("Exiting the Calorie Calculator.")
-                    logged_in = False  # Set the flag to False to exit both loops
+                    logged_in = False
                 else:
                     print("Invalid option. Please choose a valid option.")
     elif option == "3":
         print("\nThank you for using our Calorie and BMI Calculator!")
-        break  # Exit the main loop
+        break
     else:
         print("Invalid option. Please choose a valid option.")
+
